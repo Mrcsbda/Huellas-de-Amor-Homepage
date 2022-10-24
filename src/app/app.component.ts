733f19616c15b69cad5a846f0c83371e6b4c9591
Mrcsbda <macbedoya@gmail.com>
2022-10-24 16:26:05 -0500

    sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'paginaPrincipal';
+
+  activeSidenav: boolean = false;
+  getActiveSidenav(event:boolean) {
+    this.activeSidenav = event;
+  }
+  
 }
--- src/app/common-components/navbar/navbar.component.ts
+++ src/app/common-components/navbar/navbar.component.ts
@@ -1,47 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, EventEmitter, Output } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
+export class NavbarComponent {
 
-export class NavbarComponent implements OnInit {
-  
   elements: NavbarElements[] = [
-  {
-    label: "Inicio",
-    route: ""
-  },
-  {
-    label: "Adopta",
-    route: "adopta"
-  },
-  {
-    label: "Alianzas",
-    route: "alianzas"
-  },
-  {
-    label: "Ayudanos",
-    route: "ayudanos"
-  },
-  {
-    label: "Contacto",
-    route: "contacto"
+    {
+      label: 'Inicio',
+      route: '',
+    },
+    {
+      label: 'Adopta',
+      route: 'adopta',
+    },
+    {
+      label: 'Alianzas',
+      route: 'alianzas',
+    },
+    {
+      label: 'Ayudanos',
+      route: 'ayudanos',
+    },
+    {
+      label: 'Contacto',
+      route: 'contacto',
+    },
+  ];
+  active:boolean = false;
+
+  @Output() activeSidenav = new EventEmitter<boolean>()
+  @ViewChild('sidenav') sidenav!:MatSidenav;
+
+  reason = '';
+
+  close(reason: string) {
+    this.reason = reason;
+    this.sidenav.close();
+    this.active = false;
+    this.activeSidenav.emit(false);
+  };
+
+  openSidenav() {
+    this.sidenav.open();
+    this.active = true;
+    this.activeSidenav.emit(true);
   }
-];
-
-showFiller = false;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
 
-
-interface NavbarElements{
-  label: string,
-  route: string
-  }
+interface NavbarElements {
+  label: string;
+  route: string;
+}
